b3ed6e047afd7cfaae5d4859edb5af499b30d6bb
Adriano Guedes <adriano.sgs@gmail.com>
2022-04-24 17:29:16 -0300

    primeiro codigo teste

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.WriteLine("primeiro teste");
+﻿Console.Clear();
+
+Console.Write("Digite o Primeiro valor: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Digite o Segundo valor: ");
+int num2 = int.Parse(Console.ReadLine());
+
+var soma = num1 + num2;
+
+Console.WriteLine($"O valor da soma é: {soma}");
+
+
+

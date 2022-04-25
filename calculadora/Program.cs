89b5b5dbc0b30af0529a8967209be702be3a11a6
Adriano Guedes <adriano.sgs@gmail.com>
2022-04-25 09:14:14 -0300

    soma e subrtrção concluidos

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,13 +1,57 @@
-﻿Console.Clear();
+﻿
+static void Soma()
+{
+  Console.Clear();
 
-Console.Write("Digite o Primeiro valor: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.Write("Digite o Segundo valor: ");
-int num2 = int.Parse(Console.ReadLine());
+  Console.Write("Digite o Primeiro valor: ");
+  var som1 = int.Parse(Console.ReadLine());
+  Console.Write("Digite o Segundo valor: ");
+  var som2 = int.Parse(Console.ReadLine());
 
-var soma = num1 + num2;
+  Console.Write("");
+  var soma = som1 + som2;
 
-Console.WriteLine($"O valor da soma é: {soma}");
+  Console.WriteLine($"O valor da soma é: {soma}");
+  Console.ReadKey();
+}
 
+// Soma();
+
+
+static void Subtracao()
+{
+  Console.Clear();
+
+  Console.Write("Digite o Primeiro valor: ");
+  var sub1 = int.Parse(Console.ReadLine());
+
+  Console.Write("Digite o Segundo Valor: ");
+  var sub2 = int.Parse(Console.ReadLine());
+
+  Console.Write("");
+
+  var subFinal = sub1 - sub2;
+  Console.Write($"O resultado da subtracao é: {subFinal}");
+
+  Console.ReadKey();
+}
+
+// Subtracao();
+
+static void Divisao()
+{
+  Console.Clear();
+
+  Console.Write("Digite o Primeiro Valor: ");
+  var div1 = int.Parse(Console.ReadLine());
+  Console.Write("Digite o Segundo Valor: ");
+  var div2 = int.Parse(Console.ReadLine());
+  Console.Write("");
+
+  var divFinal = div1 / div2;
+
+
+
+}
 
 

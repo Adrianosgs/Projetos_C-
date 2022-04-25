24fdc0d5e15efd5309b91825bb6c825caeffeb40
Adriano Guedes <adriano.sgs@gmail.com>
2022-04-25 17:00:53 -0300

    cronometro concluido

--- cronometro/Program.cs
+++ cronometro/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿static void Menu()
+{
+  Console.Clear();
+  Console.WriteLine("S = Segundos | Ex: 10s");
+  Console.WriteLine("M = Minutos | Ex: 10m");
+  Console.WriteLine("0 = Sair");
+  Console.WriteLine(" ");
+  Console.Write("Quanto tempo deseja contar: ");
+
+  string data = Console.ReadLine().ToLower();
+  char type = char.Parse(data.Substring(data.Length - 1, 1));
+  int time = int.Parse(data.Substring(0, data.Length - 1));
+  int multMinuto = 1;
+
+  if (type == 'm')
+  {
+    multMinuto = 60;
+
+    if (time == 0)
+    {
+      System.Environment.Exit(0);
+    }
+  }
+
+  Start(time * multMinuto);
+
+}
+
+static void Start(int time)
+{
+  int minutoAtual = 0;
+
+  while (minutoAtual != time)
+  {
+    Console.Clear();
+    minutoAtual++;
+    Console.WriteLine(minutoAtual);
+    Thread.Sleep(1000);
+  }
+  Console.Clear();
+  Console.WriteLine("Cronometro Finalizado");
+  Thread.Sleep(2500);
+  Menu();
+}
+
+// Start(10);
+
+
+
+Menu();

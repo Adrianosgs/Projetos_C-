c40b67ae5a03eafed2d5039a39f7e64804bbc449
Adriano Guedes <adriano.sgs@gmail.com>
2022-05-02 21:00:47 -0300

    terminando por hoje

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -9,9 +9,15 @@ namespace EditorHtml
       Console.ForegroundColor = ConsoleColor.Black;
 
       Screen();
+      Write();
+
+      var option = short.Parse(Console.ReadLine());
     }
 
     public static void Screen()
+
+
+
     {
       Console.Write("+");
       for (int i = 0; i <= 30; i += 1)
@@ -40,5 +46,25 @@ namespace EditorHtml
       Console.Write("\n");
 
     }
+
+    public static void Write()
+    {
+      Console.SetCursorPosition(3, 2);
+      Console.WriteLine("Editor Html");
+      Console.SetCursorPosition(3, 3);
+      Console.WriteLine("-----------------------");
+      Console.SetCursorPosition(3, 4);
+      Console.WriteLine("Selecione das opções");
+      Console.SetCursorPosition(3, 6);
+      Console.WriteLine("1 - Novo Arquivo");
+      Console.SetCursorPosition(3, 7);
+      Console.WriteLine("2 - Abrir");
+      Console.SetCursorPosition(3, 9);
+      Console.WriteLine("0 - Sair");
+      Console.SetCursorPosition(3, 11);
+      Console.Write("Opção: ");
+
+    }
+
   }
 }
